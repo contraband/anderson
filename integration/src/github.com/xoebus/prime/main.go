a2d704b90b7dad18877b395b2474921d2063218f
Chris Brown <xoebus@xoeb.us>
2014-08-01 01:54:11 +0100

    clean out integration because I made them wrong

--- integration/src/github.com/xoebus/prime/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	_ "github.com/xoebus/blacklist"
-	_ "github.com/xoebus/greylist-approve"
-	_ "github.com/xoebus/greylist-unknown"
-	_ "github.com/xoebus/no-license"
-	_ "github.com/xoebus/whitelist"
-)
