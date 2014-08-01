e36d81520ef56e544b6de0af1dfe6a9f6e59ed97
Chris Brown <xoebus@xoeb.us>
2014-08-01 01:54:43 +0100

    add missing integration files

---
+++ integration/src/github.com/xoebus/prime/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	_ "github.com/xoebus/blacklist"
+	_ "github.com/xoebus/greylist-approve"
+	_ "github.com/xoebus/greylist-unknown"
+	_ "github.com/xoebus/no-license"
+	_ "github.com/xoebus/whitelist"
+)
